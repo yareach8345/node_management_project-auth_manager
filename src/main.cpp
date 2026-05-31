5ed67b09ecc4d4f6149cef05a8e8a3e019a4e60b
yareach <mine8345@naver.com>
2026-05-31 22:26:37 +0900

    feature - 테스트를 위한 임시코드 작성

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,35 @@
 #include <QApplication>
 #include <QPushButton>
 
+#include <openssl/evp.h>
+#include <openssl/pem.h>
+#include <openssl/err.h>
+
 int main(int argc, char *argv[]) {
+    ERR_load_crypto_strings();
+    OpenSSL_add_all_algorithms();
+
+    EVP_PKEY_CTX *ctx = EVP_PKEY_CTX_new_id(EVP_PKEY_RSA, nullptr);
+    if (ctx == nullptr) { return -1; }
+
+    if (EVP_PKEY_keygen_init(ctx) <= 0) { return -1; }
+    if (EVP_PKEY_CTX_set_rsa_keygen_bits(ctx, 2048) <= 0) { return -1; }
+
+    EVP_PKEY *pkey = nullptr;
+    if (EVP_PKEY_keygen(ctx, &pkey) <= 0) { return -1; }
+
+    FILE *priv_file = fopen("root_private_key.pem", "wb");
+    if (priv_file) {
+        qDebug() << "???";
+        PEM_write_PrivateKey(priv_file, pkey, nullptr, nullptr, 0, nullptr, nullptr);
+        fclose(priv_file);
+    }
+    FILE *pub_file = fopen("root_public_key.pem", "wb");
+    if (pub_file) {
+        PEM_write_PrivateKey(pub_file, pkey, nullptr, nullptr, 0, nullptr, nullptr);
+        fclose(pub_file);
+    }
+
     QApplication a(argc, argv);
     QPushButton button("Hello world!", nullptr);
     button.resize(200, 100);
